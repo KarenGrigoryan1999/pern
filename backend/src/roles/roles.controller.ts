554dc835b55967c02bcaba9bd8fd2ceffd8db963
KarenGrigoryan1999 <41522488+KarenGrigoryan1999@users.noreply.github.com>
2023-08-15 00:53:39 +0300

    Delete backend directory

--- backend/src/roles/roles.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { RolesService } from './roles.service';
-
-@Controller('role')
-export class RolesController {
-  constructor(private readonly rolesService: RolesService) {}
-}
